a3ec863edc7a5d8c0a316042ba9b6cd21b8b0e8b
Anil Barwar <87175161+Anilbarwar@users.noreply.github.com>
2025-05-13 13:06:39 +0530

    Update WebSocketConfig.java

--- src/main/java/com/game/loot/config/WebSocketConfig.java
+++ src/main/java/com/game/loot/config/WebSocketConfig.java
@@ -19,7 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://192.168.1.6:3000", "http://localhost:3000", "https://lootft.loca.lt", "https://loot-production.up.railway.app/", "https://loot-frontend-one.vercel.app")
+                .setAllowedOrigins("http://192.168.1.6:3000", "http://localhost:3000", "https://lootft.loca.lt", "https://loot-production.up.railway.app/", "https://loot-frontend-one.vercel.app",
+                                  "https://loot-frontend-git-render-anil-barwars-projects.vercel.app/")
                 .setAllowedOriginPatterns("*")
                 .withSockJS()
                 .setSessionCookieNeeded(true);
